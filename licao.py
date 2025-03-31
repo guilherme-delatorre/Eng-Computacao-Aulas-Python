5ec5d32643f85a701e92322a5970b4c50debff8e
guilherme-delatorre <gjh.delatorre@gmail.com>
2025-03-31 17:43:05 -0300

    Add files via upload

---
+++ licao.py
@@ -0,0 +1,56 @@
+#a= float(input('quantos metros: '))
+#= a*1000
+#print (f'{a} metros = {b:.2f} milimetros')
+
+#a= int(input('Quantos dias?: '))
+#b= int(input('Quantas horas?: '))
+#c= int(input('Quantos minutos?: '))
+#e= int(input('Quantos segundos?: '))
+
+#f= a*86400
+#g= b*3600
+#h= c*60
+
+#print (f'o total de segundos é:{f+g+h+e}')
+
+#a= int(input ('qual o salário?:'))
+#b= int(input ('qual a porcentagem do aumento?: '))
+
+#c= a * b / 100
+#d= a + c
+#print (f'o aumento foi de {c:.0f} e o novo salário é {d:.0f}')
+
+#a = float(input ('qual o valor da mercadoria?'))
+#b = int(input ('qual o percentual de desconto?'))
+#c = a * b / 100
+#print (f'o desconto é: {c}')
+#print (f'o valor a pagar é: {a - c}')
+
+#a = float(input('qual a distancia a percorrer em km?'))
+#b = int(input('qual a velocidade media esperada?'))
+
+#print (f'vai demorar {a / b} horas')
+
+
+#a = int(input('qual a temperatura em C?'))
+#B = (a*1.8) + 32
+#print(f'{a} Celsius = {B:.1f}')
+
+
+#a = float(input('quantos km percorreu?'))
+#b = int(input('quantos dias alugou?'))
+#c = (b*60)+(a*0.15)
+#print(f"voce deve pagar R${c:.2f}")
+
+
+#a = float(input('x='))
+#b = float(input('y='))
+
+#z = (a**2 + b**2) / (a - b)**2
+#print(f'{z:.2f}')
+
+
+#a = float(input('qual o salario? '))
+#b = ((a*35) / 100) + a
+#print (f'{b:.2f}')
+
